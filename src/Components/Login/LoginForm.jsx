499b5897b4b3b3ffcce3266298ab919cc0728ffe
dgusfr <diegogustavo.fr@gmail.com>
2024-05-10 08:01:58 -0300

    add autologin and logout

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -9,7 +9,7 @@ const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  const { userLogin, data } = React.useContext(UserContext);
+  const { userLogin, data, error, loading } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -25,7 +25,13 @@ const LoginForm = () => {
       <form action="" onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
         <Button>Entrar</Button>
+        {error && <p>{error}</p>}
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,5 +1,11 @@
-import React, { Children } from "react";
-import { TOKEN_POST, USER_GET } from "./api";
+import React, { Children, useCallback, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  TOKEN_POST,
+  TOKEN_VALIDATE_POST,
+  TOKEN_VALIDATE_POST,
+  USER_GET,
+} from "./api";
 
 export const UserContext = React.createContext();
 
@@ -8,6 +14,20 @@ export const UserStorage = ({ Children }) => {
   const [login, setLogin] = React.useState(null);
   const [loading, setLoading] = React.useState(null);
   const [error, setError] = React.useState(null);
+  const navigate = useNavigate();
+
+  //resetando metricas ao valor inicial para o logout do usario
+  const userLogout = useCallback(
+    async function () {
+      setData(null);
+      setError(null);
+      setLoading(false);
+      setLogin(false);
+      window.localStorage.removeItem("token");
+      navigate("/login");
+    },
+    [navigate]
+  );
 
   //Etapa de realização do login
   async function getUser(token) {
@@ -19,16 +39,53 @@ export const UserStorage = ({ Children }) => {
   }
 
   async function userLogin(username, password) {
-    const { url, options } = TOKEN_POST({ username, password });
-    const tokenRes = await fetch(url, options);
-    //Extraindo o valor da chave token por desestruturação
-    const { token } = await tokenRes.json();
-    window.localStorage.setItem("token", token);
-    getUser(token);
+    try {
+      setError(null);
+      setLoading(true);
+      const { url, options } = TOKEN_POST({ username, password });
+      const tokenRes = await fetch(url, options);
+      if (!tokenRes.ok) throw new Error(`Error: ${tokenRes.statusText}`);
+      //Extraindo o valor da chave token por desestruturação
+      const { token } = await tokenRes.json();
+      window.localStorage.setItem("token", token);
+      await getUser(token);
+      navigate("/conta");
+    } catch (err) {
+      setError(err.message);
+      setLogin(false);
+    } finally {
+      setLoading(false);
+    }
   }
+  //Autologin, usuario permanece logado mesmo se recarregar a página.
+  useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getIten("token");
+      if (token) {
+        try {
+          // setError e setLoading inicializam desta maneira quando o suario faz o login:
+          setError(null);
+          setLoading(true);
+          const { url, options } = TOKEN_VALIDATE_POST(token);
+          const response = await fetch(url, options);
+          //throw new Error() interrompe a execução do código e sinalizando que algo deu errado.
+          if (!response.ok) throw new Error("token invalido");
+          await getUser(token);
+          const json = await response.json();
+        } catch (err) {
+          userLogout();
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+    autoLogin();
+  }, [userLogout()]);
 
   return (
-    <UserContext.Provider value={{ userLogin, data }}>
+    <UserContext.Provider
+      value={{ userLogin, userLogout, data, error, loading, login }}
+    >
       {Children}
     </UserContext.Provider>
   );
