e4c2ed60bbfe2859fd37ac677a663f4dbd81688e
dgusfr <diegogustavo.fr@gmail.com>
2024-06-08 08:00:52 -0300

    add footer component

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -14,7 +14,6 @@ const User = () => {
   return (
     <section className="container">
       <Head title="Minha Conta" />
-
       <UserHeader />
       <Routes>
         <Route path="/" element={<Feed user={data.id} />} />
