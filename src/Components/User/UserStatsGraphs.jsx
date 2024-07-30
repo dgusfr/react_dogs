9447963a2d7c882718d45d10c48ec0477b1f976d
dgusfr <diegogustavo.fr@gmail.com>
2024-07-30 12:23:41 -0300

    changes

--- src/Components/User/UserStatsGraphs.jsx
+++ src/Components/User/UserStatsGraphs.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styles from './UserStatsGraphs.module.css';
-import { VictoryPie, VictoryChart, VictoryBar } from 'victory';
+import React from "react";
+import styles from "./UserStatsGraphs.module.css";
+import { VictoryPie, VictoryChart, VictoryBar } from "victory";
 
 const UserStatsGraphs = ({ data }) => {
   const [graph, setGraph] = React.useState([]);
@@ -15,7 +15,7 @@ const UserStatsGraphs = ({ data }) => {
     });
 
     setTotal(
-      data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b, 0),
+      data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b, 0)
     );
     setGraph(graphData);
   }, [data]);
@@ -33,12 +33,12 @@ const UserStatsGraphs = ({ data }) => {
           style={{
             data: {
               fillOpacity: 0.9,
-              stroke: '#fff',
+              stroke: "#fff",
               strokeWidth: 2,
             },
             labels: {
               fontSize: 14,
-              fill: '#333',
+              fill: "#333",
             },
           }}
         />
